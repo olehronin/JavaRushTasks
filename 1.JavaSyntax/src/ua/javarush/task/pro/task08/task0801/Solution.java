f7419d08e7518201a06ffcce56cd1c6205b0f722
olehronin <goddemmet@ukr.net>
2023-10-12 20:40:17 +0300

    task8_12_10_23

--- 1.JavaSyntax/src/ua/javarush/task/pro/task08/task0801/Solution.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task08/task0801/Solution.java
@@ -10,16 +10,17 @@ public class Solution {
 
     public static double sin(double a) {
         //напишіть тут ваш код
-        return 0;
+
+        return Math.sin(toRadians(a));
     }
 
     public static double cos(double a) {
         //напишіть тут ваш код
-        return 0;
+        return Math.cos(toRadians(a));
     }
 
     public static double tan(double a) {
         //напишіть тут ваш код
-        return 0;
+        return Math.tan(toRadians(a));
     }
 }
