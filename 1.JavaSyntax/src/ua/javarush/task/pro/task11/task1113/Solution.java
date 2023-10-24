34ef31b36b98dd7bef328c29c9c4234406779c94
olehronin <goddemmet@ukr.net>
2023-10-24 15:25:18 +0300

    level12_task1113

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1113/Solution.java
@@ -0,0 +1,13 @@
+package ua.javarush.task.pro.task11.task1113;
+
+/* 
+Слідкуємо за дужками
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
