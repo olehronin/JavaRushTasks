01298b4dbf2de68037c68f1a0ae3575dcd19a1a9
olehronin <goddemmet@ukr.net>
2023-10-24 15:34:07 +0300

    level12_task1113

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1114/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.pro.task11.task1114;
+
+/* 
+І знову дужки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 1;
+        if (number < 5) {
+            System.out.println("number < 5");
+        } else {
+            System.out.println();
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1117/Solution.java
@@ -0,0 +1,9 @@
+package ua.javarush.task.pro.task11.task1117;/*
+Пакет потрібен?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("TestPackage");
+    }
+}
