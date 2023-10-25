fad8f738959be3ee07246b6eea31214ecfb40b9a
olehronin <goddemmet@ukr.net>
2023-10-25 16:06:25 +0300

    level13_task1205-1206

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task12/task1205/Solution.java
@@ -0,0 +1,49 @@
+package ua.javarush.task.pro.task12.task1205;
+
+/* 
+Метод ділення
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divide(1, 0);
+        divide(-1, 0);
+        divide(0, 0);
+        divide(100, 20);
+        divide(200, 20);
+    }
+
+    public static void divide(double a, double b) {
+        //напишіть тут ваш код
+        if (a > 0 && b == 0) {
+            System.out.println(Double.POSITIVE_INFINITY);
+        } else if (a == Double.POSITIVE_INFINITY && b >= 0 && b != Double.POSITIVE_INFINITY) {
+            System.out.println(Double.POSITIVE_INFINITY);
+        } else if (a == Double.NEGATIVE_INFINITY && b < 0 && b < Double.NEGATIVE_INFINITY) {
+            System.out.println(Double.POSITIVE_INFINITY);
+        } else if (a < 0 && b == 0) {
+            System.out.println(Double.NEGATIVE_INFINITY);
+        } else if (a == Double.NEGATIVE_INFINITY && b >= 0 && b < Double.NEGATIVE_INFINITY) {
+            System.out.println(Double.NEGATIVE_INFINITY);
+        } else if (a == Double.POSITIVE_INFINITY && b < 0 && b < Double.NEGATIVE_INFINITY) {
+            System.out.println(Double.NEGATIVE_INFINITY);
+        } else if (a == 0 && b == 0) {
+            System.out.println(Double.NaN);
+        } else if (Double.isNaN(a) || Double.isNaN(b)) {
+            System.out.println(Double.NaN);
+        } else if (a == Double.POSITIVE_INFINITY && b == Double.POSITIVE_INFINITY) {
+            System.out.println(Double.NaN);
+        } else if (a == Double.POSITIVE_INFINITY && b == Double.NEGATIVE_INFINITY) {
+            System.out.println(Double.NaN);
+        } else if (a == Double.NEGATIVE_INFINITY && b == Double.POSITIVE_INFINITY) {
+            System.out.println(Double.NaN);
+        } else if (a == Double.NEGATIVE_INFINITY && b == Double.NEGATIVE_INFINITY) {
+            System.out.println(Double.NaN);
+        }else {
+            System.out.println(a/b);
+        }
+
+
+    }
+}
---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task12/task1206/Solution.java
@@ -0,0 +1,53 @@
+package ua.javarush.task.pro.task12.task1206;
+
+/* 
+Аналіз рядків
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Гадаю, це буде нова фіча." +
+                "Тільки нікому не кажіть, що вона виникла випадково.";
+
+        System.out.println("Кількість цифр у рядку : " + countDigits(string));
+        System.out.println("Кількість літер у рядку : " + countLetters(string));
+        System.out.println("Кількість пробілів у рядку : " + countSpaces(string));
+    }
+
+    public static int countDigits(String string) {
+        //напишіть тут ваш код
+        int count = 0;
+        char[] strings = string.toCharArray();
+        for (int i = 0; i < strings.length; i++) {
+            if (Character.isDigit(strings[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countLetters(String string) {
+        //напишіть тут ваш код
+
+        int count = 0;
+        char[] strings = string.toCharArray();
+        for (int i = 0; i < strings.length; i++) {
+            if (Character.isLetter(strings[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countSpaces(String string) {
+        //напишіть тут ваш код
+        int count = 0;
+        char[] strings = string.toCharArray();
+        for (int i = 0; i < strings.length; i++) {
+            if (Character.isSpaceChar(strings[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
