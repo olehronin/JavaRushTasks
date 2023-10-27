ecb817a9caf2eb6cc202a69df8b29ed752573897
olehronin <goddemmet@ukr.net>
2023-10-27 17:00:39 +0300

    level13_task1218-1219

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task12/task1219/Solution.java
@@ -0,0 +1,19 @@
+package ua.javarush.task.pro.task12.task1219;
+
+import java.util.ArrayList;
+
+/* 
+Подвійні фігурні дужки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>();
+        strings.add("Так");
+        strings.add("теж");
+        strings.add("можна");
+        strings.add("робити");
+        strings.add("!");
+    }
+}
