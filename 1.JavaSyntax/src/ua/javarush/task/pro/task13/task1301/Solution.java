1e390db9b228227accb4fe18df031fd3b7a77f6c
olehronin <goddemmet@ukr.net>
2023-10-29 19:10:02 +0200

    level14_task1301

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task13/task1301/Solution.java
@@ -0,0 +1,32 @@
+package ua.javarush.task.pro.task13.task1301;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Повертаємо своє
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] array = {"Через", "три", "роки", "я", "буду", "Senior", "Java", "Developer"};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("___________________________________");
+
+        HashSet<String> hashSet = arrayToHashSet(array);
+        for(String s : hashSet) {
+            System.out.println(s);
+        }
+    }
+
+    public static HashSet<String> arrayToHashSet(String[] strings) {
+        //напишіть тут ваш код
+        HashSet<String> addAll = new HashSet<>();
+        Collections.addAll(addAll,strings);
+        return addAll;
+
+    }
+}
