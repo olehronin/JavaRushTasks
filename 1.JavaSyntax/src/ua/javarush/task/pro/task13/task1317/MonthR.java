3b3088a94414762be25c5851e180ce7c0c2d81b9
olehronin <goddemmet@ukr.net>
2023-10-31 14:47:00 +0200

    level14_task1317

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task13/task1317/MonthR.java
@@ -0,0 +1,20 @@
+package ua.javarush.task.pro.task13.task1317;
+
+/* 
+Перетворюємо enum на клас
+*/
+
+public enum MonthR {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
