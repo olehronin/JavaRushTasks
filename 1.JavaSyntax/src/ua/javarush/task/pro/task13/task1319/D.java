e4e5fa142978ad588c3293bb56632f0f5cb7f158
olehronin <goddemmet@ukr.net>
2023-10-31 16:49:00 +0200

    level14_task1319

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task13/task1319/D.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task13.task1319;
+
+import java.util.Arrays;
+
+public class D {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(Month.getWinterMonths()));
+        System.out.println(Arrays.toString(Month.getSpringMonths()));
+        System.out.println(Arrays.toString(Month.getSummerMonths()));
+        System.out.println(Arrays.toString(Month.getAutumnMonths()));
+
+    }
+}
--- 1.JavaSyntax/src/ua/javarush/task/pro/task13/task1319/Month.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task13/task1319/Month.java
@@ -4,6 +4,9 @@ package ua.javarush.task.pro.task13.task1319;
 Місяці в порах року
 */
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public enum Month {
     JANUARY,
     FEBRUARY,
@@ -19,5 +22,37 @@ public enum Month {
     DECEMBER;
 
     //напишіть тут ваш код
+
+    public static Month[] getWinterMonths() {
+        Month[] monthArray = new Month[3];
+        monthArray[0] = Month.DECEMBER;
+        monthArray[1] = Month.JANUARY;
+        monthArray[2] = Month.FEBRUARY;
+        return monthArray;
+    }
+
+    public static Month[] getSpringMonths() {
+        Month[] monthArray = new Month[3];
+        monthArray[0] = Month.MARCH;
+        monthArray[1] = Month.APRIL;
+        monthArray[2] = Month.MAY;
+        return monthArray;
+    }
+
+    public static Month[] getSummerMonths() {
+        Month[] monthArray = new Month[3];
+        monthArray[0] = Month.JUNE;
+        monthArray[1] = Month.JULY;
+        monthArray[2] = Month.AUGUST;
+        return monthArray;
+    }
+
+    public static Month[] getAutumnMonths() {
+        Month[] monthArray = new Month[3];
+        monthArray[0] = Month.SEPTEMBER;
+        monthArray[1] = Month.OCTOBER;
+        monthArray[2] = Month.NOVEMBER;
+        return monthArray;
+    }
 
 }
