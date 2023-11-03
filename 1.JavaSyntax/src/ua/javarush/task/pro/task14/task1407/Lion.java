2a22d2277fa2604e9a195e31231d2c8c20c35f89
olehronin <goddemmet@ukr.net>
2023-11-03 10:25:14 +0200

    level15_task1407

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task14/task1407/Lion.java
@@ -0,0 +1,24 @@
+package ua.javarush.task.pro.task14.task1407;
+
+/* 
+Їжа їжею, а сон за розкладом
+*/
+
+public class Lion {
+
+    public void eat(Food food) {
+        try {
+            System.out.println();
+            System.out.println("шукає їжу");
+            System.out.println("знайшов " + food.name);
+            System.out.println("усе з'їв");
+        } catch (NullPointerException e) {
+            System.out.println("нічого не знайшов");
+            return;
+        } //напишіть тут ваш код
+        finally {
+            System.out.println("ліг спати" + (food == null ? " голодним" : ""));
+
+        }
+    }
+}
