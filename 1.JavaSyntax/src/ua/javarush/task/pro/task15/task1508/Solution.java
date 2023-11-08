920af70ece9ebb9e594530725244febd800359f8
olehronin <goddemmet@ukr.net>
2023-11-08 10:44:25 +0200

    level15_task1508

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task15/task1508/Solution.java
@@ -0,0 +1,29 @@
+package ua.javarush.task.pro.task15.task1508;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Читаємо з консолі
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            char[] chars = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (i % 2 == 1) {
+                    System.out.print(String.valueOf(chars[i]).toUpperCase());
+                } else {
+                    System.out.print(String.valueOf(chars[i]).toLowerCase());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+
