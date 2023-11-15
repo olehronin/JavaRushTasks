6fc6662987e41cd67454f23b87fbd93e5c6507f7
olehronin <goddemmet@ukr.net>
2023-11-15 17:18:39 +0200

    level11_task1705

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1705/Cat.java
@@ -0,0 +1,7 @@
+package ua.javarush.task.pro.task17.task1705;
+
+public class Cat extends Astronaut {
+    public String getInfo() {
+        return "Кіт";
+    }
+}
---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1705/Human.java
@@ -0,0 +1,7 @@
+package ua.javarush.task.pro.task17.task1705;
+
+public class Human extends Astronaut {
+    public String getInfo() {
+        return "Людина";
+    }
+}
