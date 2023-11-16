445f2da57c9b9911d97c07927ac3bb571997bfd7
olehronin <goddemmet@ukr.net>
2023-11-16 10:25:48 +0200

    level11_task1709

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,13 @@
+package ua.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактні фігури
+*/
+
+public abstract class Shape {
+    //напишіть тут ваш код
+
+    public abstract double getPerimeter();
+    public abstract double getArea();
+
+}
