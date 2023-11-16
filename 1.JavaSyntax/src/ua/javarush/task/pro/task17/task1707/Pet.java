65aa1281f3a743387e9293b185d80663a91e92a5
olehronin <goddemmet@ukr.net>
2023-11-16 07:34:42 +0200

    level11_task1707

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1707/Pet.java
@@ -0,0 +1,7 @@
+package ua.javarush.task.pro.task17.task1707;
+
+public class Pet {
+    public void printInfo(){
+        System.out.println("Я домашній вихованець.");
+    }
+}
---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1707/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task17.task1707;
+
+/* 
+Вихованці бувають різними
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.printInfo();
+        Dog dog = new Dog();
+        dog.printInfo();
+    }
+}
