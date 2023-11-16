71c4a12b26576fd19f8b39961aee0de796871f3b
olehronin <goddemmet@ukr.net>
2023-11-16 12:02:35 +0200

    level11_task1713

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1713/Artist.java
@@ -0,0 +1,8 @@
+package ua.javarush.task.pro.task17.task1713;
+
+/* 
+Хай розпочнеться вистава!
+*/
+
+public interface Artist extends Dance, Sing{
+}
---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1713/Dance.java
@@ -0,0 +1,4 @@
+package ua.javarush.task.pro.task17.task1713;
+
+public interface Dance {
+}
